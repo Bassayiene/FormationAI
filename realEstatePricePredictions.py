29b66e437aa5f0c84d646aa1c688e4dc0f6f659b
nour el yakine bassayiene <nourelyakine.bassayiene2@uit.ac.ma>
2025-10-05 11:59:20 +0100

    Exercise 11

---
+++ realEstatePricePredictions.py
@@ -0,0 +1,14 @@
+import numpy as np
+# input values for three mökkis: size, size of sauna, distance to water, number of indoor bathrooms, 
+# proximity of neighbors
+X = [[66, 5, 15, 2, 500], 
+     [21, 3, 50, 1, 100], 
+     [120, 15, 5, 2, 1200]]
+c = [3000, 200, -50, 5000, 100]    # coefficient values
+
+def predict(X, c):
+    for x in X :
+        price =  np.array(x) @ np.array(c)
+        print(price)
+
+predict(X, c)
